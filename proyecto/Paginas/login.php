781935480f0bc9db6d504f710e5ae23311e2c479
AnaLeslie <45467533+AnaLeslie@users.noreply.github.com>
2018-11-29 13:04:51 -0600

    Update login.php

--- proyecto/Paginas/login.php
+++ proyecto/Paginas/login.php
@@ -29,7 +29,7 @@
 <meta name="viwport" content="width=device-width, initial-scal=1,maximum-scale=1, height=device-height, user-scale=true/false">
 <style >
   h1{
-     color: red;
+     color: blue;
   }
 </style>
 
