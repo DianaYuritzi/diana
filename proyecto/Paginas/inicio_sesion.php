015f6e18aa4a07744c040c170741c1837f067d71
DianaYuritzi <45374785+DianaYuritzi@users.noreply.github.com>
2018-12-07 02:48:05 -0600

    Update inicio_sesion.php

--- proyecto/Paginas/inicio_sesion.php
+++ proyecto/Paginas/inicio_sesion.php
@@ -1,68 +1,123 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <title>Squash Tolcayuca</title>
 <meta name="keywords" content="" />
 <meta name="description" content="" />
+<link href="paginas/styles.css" rel="stylesheet" type="text/css" />
+<link href="paginas/styless.css" rel="stylesheet" type="text/css" />
+<link href="paginas/resto.css" rel="stylesheet" type="text/css" />
+
 <meta name="viwport" content="width=device-width, initial-scal=1,maximum-scale=1, height=device-height, user-scale=true/false">
-<link href="styles.css" rel="stylesheet" type="text/css" />
+<style >
+.color{
+    color:white;
+}
+  h1{
+     color: #2E86C1;
+  }
+  h2{
+     color: black;
+  }
+  table{
+
+background-color:  #2E86C1; opacity: 0.9;
+
+  }
+  .texto{
+   
+  font-size: 2.0em;
+
+float: right;
+   
+  }
+</style>
+
 </head>
-<body>
+<body class="color">
 
 
-<div class="top">
+<div class="wrapper">
+  <input checked type=radio name="slider" id="slide1" />
+  <input type=radio name="slider" id="slide2" />
+  <input type=radio name="slider" id="slide3" />
+  <input type=radio name="slider" id="slide4" />
+  <input type=radio name="slider" id="slide5" />
 
-<ul class="round">
-			<li><img src="images/header1.jpg" width="500" height="280" /></li>
-			<li><img src="images/header2.jpg" width="500" height="280"/></li>
-			<li><img src="images/header3.jpg" width="500" height="280" /></li>
-			<li><img src="images/header4.jpg" width="500" height="280" /></li>
-			<li><img src="images/header5.jpg" width="500" height="280" /></li>
-			<li><img src="images/header6.jpg" width="500" height="280" /></li>
-</ul>
-<script type="text/javascript" src="lib/jquery.js"></script>
-<script type="text/javascript" src="lib/jquery.roundabout.js"></script>
-<script type="text/javascript">
-			
-			$(document).ready(function() {
-				$('.round').roundabout();
-			});
-		
-		</script>
+  <div class="slider-wrapper">
+    <div class=inner>
+      <article>
+        <div class="info top-left">
+          <h3>JUEGA CON AMIGOS</h3>
+        </div>
+       <img src="paginas/images/header2.jpg" />
+      </article>
+
+      <article>
+        <div class="info bottom-right">
+          <h3>HAZ EJERCICIO</h3>
+        </div>
+        <img  src="paginas/images/header4.jpg"/>
+      </article>
+
+      <article>
+        <div class="info bottom-left">
+          <h3>DIVIERTETE</h3>
+        </div>
+        <img src="paginas/images/header3.jpg" />
+      </article>
+
+      <article>
+        <div class="info top-right">
+          <h3>ENTRENA</h3>
+        </div>
+        <img src="paginas/images/header5.jpg" />
+      </article>
+
+      <article>
+        <div class="info bottom-left">
+          <h3>ESFUERZATE</h3>
+        </div>
+        <img src="paginas/images/header6.jpg" />
+      </article>
+    </div>
+    <!-- .inner -->
+  </div>
+  <!-- .slider-wrapper -->
+
+  <div class="slider-prev-next-control">
+    <label for=slide1></label>
+    <label for=slide2></label>
+    <label for=slide3></label>
+    <label for=slide4></label>
+    <label for=slide5></label>
+  </div>
+  <!-- .slider-prev-next-control -->
+
+  <div class="slider-dot-control">
+    <label for=slide1></label>
+    <label for=slide2></label>
+    <label for=slide3></label>
+    <label for=slide4></label>
+    <label for=slide5></label>
+  </div>
+  <!-- .slider-dot-control -->
 </div>
 
         <!-- content begins -->
-        		<div class="cont_top"></div>
-       			<div id="content">
-                    <div class="cont_home">
-                    	<div class="home_box">
-
-                        
-                            <div style="height:15px"></div>
-                        	<img src="images/img11.jpg" alt="" />
-                          <div style="height:10px;"></div>
-                        	
-                          <div style="height:5px"></div>
-                        </div><div style="width: 40px; height:20px; float: left;"></div>
-                        <div class="home_box">
-
-                        	<h1>Bienvenido a 'Squash Tolcayuca'</h1>
-                            <div style="height:15px"></div>
-                        	<div style="height:10px;"></div>
-                        	El squash es un deporte de raqueta que se practica en interiores con dos jugadores y una pelota de goma que puede tener distintos grados de velocidad o rebote. El grado de velocidad y/o rebote se identifica en el color de la pelota.
-                          <div style="height:5px"></div>
+        <center><table class="tabla" width="70%">
+    <tr>
+      <td align="left"> <img src="paginas/images/img8.jpg" width="100%"></td>
+      <td class="texto"> <h2> <center> <b>Bienvenido</b> </center></h2><br>
+       <center><h2><?php if(isset($_SESSION['NOMBRE'])) {echo $_SESSION['NOMBRE'];}?> Club Squash Tolcayuca</h2><br> </center>
+      <p>El squash es un deporte de raqueta que se practica en interiores con dos jugadores y una pelota de goma que puede tener distintos grados de velocidad o rebote. El grado de velocidad y/o rebote se identifica en el color de la pelota.</p>
+          
+    </td>
+    </tr>
+  </table>
+  </center>	
  
-                      </div><div style="width: 40px; height:20px; float: left;"></div>
-                      <div class="home_box">
-                            <div style="height:15px"></div>
-                        	<img src="images/img13.jpg" alt="" />
-                            <div style="height:10px;"></div>
-                            <div style="height:5px"></div>
-                      </div>
-                        
-                   	  <div style="clear: both"></div>
-                    </div>
-        		</div>
-                <div class="cont_bot"></div>
+            
+  </body>
+            </body>
 </html>
